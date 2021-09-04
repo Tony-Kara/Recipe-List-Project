9b9c037d7825545a286b35d76218014f91059bfd
Tony-Kara <eniolaanthony@gmail.com>
2021-09-04 23:20:30 +0600

    Initial Commit

---
+++ Recipe List Project/Recipe_List_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_ProjectApp.swift
+//  Recipe List Project
+//
+//  Created by mac on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
